09e6443c5ae3f3520bf037d0bc28896811bf1d9c
Kenneth Hautea <lichking20011@gmail.com>
2019-04-06 21:58:51 +0800

    modaladd

--- finalphp/supplier.php
+++ finalphp/supplier.php
@@ -37,35 +37,51 @@
 <div class="container-fluid">
 	<div class="row">
   	<div class="col-sm-4 view">
-		<button type="button" class="btn btn-primary" href="#" >View Delivery</button>
+		  <button type="button" class="btn btn-primary" href="#" >View Delivery</button>
 		</div>
   	<div class="col-sm-4"></div>
   	<div class="col-sm-4 group">
 			<div>
-				<button type="button" class="btn btn-success" href="#" data-toggle="modal" data-target="#myModal" >Add</button>
+				<button type="button" class="btn btn-success" href="#" data-toggle="modal" data-target="#suppadd" >Add</button>
 				<button type="button" class="btn btn-warning" href="#" >Update</button>
 				<button type="button" class="btn btn-danger" href="#" >Delete</button>
 			</div>
-
 		</div>
+  </div>
 </div>
-	</div>
 </div>
-<!-- Modal -->
-<div id="myModal" class="modal fade" role="dialog">
+<!-- Modal add-->
+<div id="suppadd" class="modal fade" role="dialog">
   <div class="modal-dialog">
-
-    <!-- Modal content-->
+    <!-- content-->
     <div class="modal-content">
       <div class="modal-header">
         <button type="button" class="close" data-dismiss="modal">&times;</button>
-        <h4 class="modal-title">Modal Header</h4>
+        <h4 class="modal-title">Add Supplier</h4>
       </div>
       <div class="modal-body">
-        <p>Some text in the modal.</p>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+        <form>
+            <div class="form-group">
+              <label for="text">ID</label>
+              <input type="text" class="form-control" id="text">
+            </div>
+            <div class="form-group">
+              <label for="text">Name</label>
+              <input type="text" class="form-control" id="text">
+            </div>
+            <div class="form-group">
+              <label for="text">Contact Number</label>
+              <input type="text" class="form-control" id="text">
+            </div>
+            <div class="form-group">
+              <label for="text">Address</label>
+              <input type="text" class="form-control" id="text">
+            </div>
+            <div>
+              <button type="submit" class="btn btn-success">Save</button>
+              <button type="submit" class="btn btn-danger">Cancel</button>
+            </div>
+        </form>
       </div>
     </div>
 
